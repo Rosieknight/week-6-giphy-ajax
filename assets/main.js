d767a936a3c4cf5a6fb57efd5446da276fd59d5d
Laura E <Laura E>
2017-02-28 23:50:44 -0500

    It's working! It's working!

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,14 @@
 //Theme is "Movies I've seen."
-var starter =["The Lego Movie", "Star Wars", "Ghostbusters", "Lord of the Rings", "Robocop", "Jurassic Park", "Terminator 2", "The Pincess Bride", "Back to the Future", "Batman", "Harry Potter", "Men In Black", "Hidden Figures", "Captain America: The First Avenger", "Indiana Jones and the Last Crusade", "Muppet Treasure Island", "Labyrinth", "Superman", "Spaceballs", "2001: A Space Odyssey", "Watership Down", "Nightmare Before Christmas", "The Sixth Sense", "Rogue One: Star Wars Story"];
+var topics =["The Lego Movie", "Star Wars", "Ghostbusters", "Lord of the Rings", "Robocop", "Jurassic Park", "Terminator 2", "The Pincess Bride", "Back to the Future", "Batman", "Harry Potter", "Men In Black", "Hidden Figures", "Captain America: The First Avenger", "Indiana Jones and the Last Crusade", "Muppet Treasure Island", "Labyrinth", "Superman", "Spaceballs", "2001: A Space Odyssey", "Watership Down", "Nightmare Before Christmas", "The Sixth Sense", "Rogue One: Star Wars Story"];
 
 //Setting up a function to make the buttons.
 function buttonMaker(){
 	$("#buttons").empty();
-	for (var i = 0; i < starter.length; i++) {
+	for (var i = 0; i < topics.length; i++) {
 		var actualButtons =$("<button>");
 		actualButtons.attr("class","btn-primary movies");
-		actualButtons.attr("id", starter[i]);
-		actualButtons.text(starter[i]);
+		actualButtons.attr("id", topics[i]);
+		actualButtons.text(topics[i]);
 		$("#buttons").append(actualButtons);
 	}
 }
@@ -19,25 +19,54 @@ $(document).ready(function(){
 	buttonMaker();
 });
 
-//What happens when adding a movie to the list. I want to clear out the textbox
-//but that's not working at the movment.
+//What happens when adding a movie to the list. The text box clears, as well.
 $(document).on("click", "#addMovie", function(event){
 	event.preventDefault();
 	var newOne = $("#newMovie").val().trim();
-	starter.push(newOne);
+	topics.push(newOne);
 	buttonMaker();
+	$("#newMovie").val("");
 });
 
-//AJAX to get the gifs.
+//Telling AJAX to get the gifs.
 $(document).on("click", ".movies", function(){
+	$("#display").empty();
 	var title = $(this).attr("id");
 	var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + title + 
-	"&api_key=dc6zaTOxFJmzC";
+	"&api_key=dc6zaTOxFJmzC&limit=10";
 	$.ajax({
 		url: queryUrl,
 		method: "GET",
 	}).done(function(response){
-		
-	})
-
+		console.log(response);
+		var results = response.data;
+		console.log(results);
+		for (var j = 0; j < results.length; j++) {
+			var pretty = $("<div>");
+			pretty.attr("class", "col-sm-4 col-lg-3 text-center");
+			var rate = $("<h4>");
+			rate.text(results[j].rating);
+			pretty.append(rate);
+			var pics = $("<img>");
+			pics.attr("src", results[j].images.fixed_width_still.url);
+			pics.attr("data-still", results[j].images.fixed_width_still.url);
+			pics.attr("data-moving", results[j].images.fixed_width.url);
+			pics.attr("data-status", "still");
+			pics.addClass("motion");
+			pretty.append(pics);
+			$("#display").append(pretty);
+		}
+	});
 });
+
+//Getting the gifs to animate and go still again.
+$(document).on("click", ".motion", function(){
+	var status = $(this).attr("data-status");	
+	if(status=="still"){
+		$(this).attr("src", ($(this).attr("data-moving")));
+		$(this).attr("data-status", "moving");
+	} else{
+		$(this).attr("src", ($(this).attr("data-still")));
+		$(this).attr("data-status", "still");
+	}
+})
